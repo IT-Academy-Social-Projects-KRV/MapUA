6c0e214849e8caa1f2ccb1a83c47979e4dafe99c
Roman Kalishuk <romawhitej@gmail.com>
2022-04-13 14:58:41 +0300

    Connected leaflet map to home page

--- frontend/src/components/Map/index.tsx
+++ frontend/src/components/Map/index.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
-import { StyledMap } from './styles';
+import { MapContainer, TileLayer, useMapEvents } from 'react-leaflet';
 
 function Map() {
-  return <StyledMap sx={{ mt: 20 }}>Map</StyledMap>;
+  function CoordsFinder() {
+    useMapEvents({
+      click(e) {
+        console.log(e.latlng);
+      }
+    });
+    return null;
+  }
+  return (
+    <MapContainer
+      center={[50.447731, 30.542721]}
+      zoom={9}
+      style={{ height: '100vh' }}
+    >
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      <CoordsFinder />
+    </MapContainer>
+  );
 }
 
 export default Map;
