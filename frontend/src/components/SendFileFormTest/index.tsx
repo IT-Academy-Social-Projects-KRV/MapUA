484617f50b8be9be5ab3d0eb0ea7e1bf38a0db67
Yuriy Soproniuk <sopronukCC@gmail.com>
2022-04-13 21:15:13 +0300

    ended up with storage

--- frontend/src/components/SendFileFormTest/index.tsx
+++ frontend/src/components/SendFileFormTest/index.tsx
@@ -1,7 +1,54 @@
-import React from 'react';
+import { Box, Button, Typography } from '@mui/material';
+import React, { useState } from 'react';
 
 function SendFileFormTest() {
-  return <div>SendFileFormTest</div>;
+  const [links, setLinks] = useState<string[]>([]);
+  const [files, setFiles] = useState<File[]>([]);
+  const handleFilesChange = (e: React.FormEvent<HTMLInputElement>) => {
+    const { files: filesLst } = e.currentTarget;
+    const filesListArr = [];
+
+    if (filesLst) {
+      for (let i = 0; i < filesLst.length; i += 1) {
+        filesListArr.push(filesLst[i]);
+      }
+
+      setFiles(filesListArr);
+    }
+  };
+
+  const handleFormSubmit = async () => {
+    const formData = new FormData();
+
+    const urls = await Promise.all(
+      files.map(async file => {
+        formData.set('file', file);
+        const res = await fetch(
+          `${process.env.REACT_APP_API_URI}/uploadImage`,
+          {
+            method: 'POST',
+            body: formData
+          }
+        );
+
+        return res.json();
+      })
+    );
+    setLinks(urls.map(itm => itm.url));
+  };
+
+  return (
+    <form>
+      <Box>Select file</Box>
+      <input name="file" type="file" multiple onChange={handleFilesChange} />
+      <Button onClick={handleFormSubmit}>Submit files</Button>
+      {links.map(url => (
+        <Typography variant="h6" key={url}>
+          URL: {url}
+        </Typography>
+      ))}
+    </form>
+  );
 }
 
 export default SendFileFormTest;
