260e677f16e21949a5a6375c02799b99fbf09620
Yuriy <41287687+steadyGuy@users.noreply.github.com>
2022-04-14 13:55:21 +0300

    Feautre 71 create storage for images (#4)
    * connect aws storage and add form stub
    
    * ended up with storage
    
    * storage backend main part done
    
    * jenkins refactor

--- frontend/src/screens/HomeScreen.tsx
+++ frontend/src/screens/HomeScreen.tsx
@@ -1,5 +1,6 @@
 import { Container } from '@mui/material';
 import Map from 'components/Map';
+import SendFileFormTest from 'components/SendFileFormTest';
 import React from 'react';
 
 function HomeScreen() {
@@ -7,6 +8,7 @@ function HomeScreen() {
     <Container>
       HomeScreen
       <Map />
+      <SendFileFormTest />
     </Container>
   );
 }
