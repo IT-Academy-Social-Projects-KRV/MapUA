7f72fc6d06b1d9c90a0bdf782e25580a2d50997e
Kulesh Aleksandr <53787083+6mahdapihka9@users.noreply.github.com>
2022-04-14 15:53:33 +0300

    consts for testing

---
+++ frontend/src/constants/develop.ts
@@ -0,0 +1,98 @@
+export const positions: [number, number][] = [
+  [49.447731, 29.542721],
+  [50.447731, 29.542721],
+  [51.447731, 29.542721],
+
+  [49.447731, 30.542721],
+  [50.447731, 30.542721],
+  [51.447731, 30.542721],
+
+  [49.447731, 31.542721],
+  [50.447731, 31.542721],
+  [51.447731, 31.542721]
+];
+
+export type locationType = {
+  title: string;
+  rate: {
+    likes: number;
+    dislikes: number;
+  };
+  coordinates: [number, number];
+};
+
+export const locations: locationType[] = [
+  {
+    title: '',
+    rate: {
+      likes: 1,
+      dislikes: 0
+    },
+    coordinates: [49.447731, 29.542721]
+  },
+  {
+    title: '',
+    rate: {
+      likes: 1,
+      dislikes: 0
+    },
+    coordinates: [50.447731, 29.542721]
+  },
+  {
+    title: '',
+    rate: {
+      likes: 1,
+      dislikes: 0
+    },
+    coordinates: [51.447731, 29.542721]
+  },
+  {
+    title: '',
+    rate: {
+      likes: 1,
+      dislikes: 0
+    },
+    coordinates: [49.447731, 30.542721]
+  },
+  {
+    title: '',
+    rate: {
+      likes: 1,
+      dislikes: 0
+    },
+    coordinates: [50.447731, 30.542721]
+  },
+  {
+    title: '',
+    rate: {
+      likes: 1,
+      dislikes: 0
+    },
+    coordinates: [51.447731, 30.542721]
+  },
+
+  {
+    title: '',
+    rate: {
+      likes: 1,
+      dislikes: 0
+    },
+    coordinates: [49.447731, 31.542721]
+  },
+  {
+    title: '',
+    rate: {
+      likes: 1,
+      dislikes: 0
+    },
+    coordinates: [50.447731, 31.542721]
+  },
+  {
+    title: '',
+    rate: {
+      likes: 1,
+      dislikes: 0
+    },
+    coordinates: [51.447731, 31.542721]
+  }
+];
