fba8a68e35ea23545fa95743143842b3391dc44d
AndriiYozh <yojuks@gmail.com>
2022-04-14 12:12:51 +0300

    Add locations controller

--- backend/controllers/LocationsController.ts
+++ backend/controllers/LocationsController.ts
@@ -4,9 +4,10 @@ import Location from "../models/Locations";
 const LocationController = {
   async getLocationId(req: Request, res: Response) {
     try {
-      const { _id } = req.body;
-      // const locations = await Locations.findById({ _id });
-      const locations = Location.find();
+      const { id } = req.body;
+
+      const locations = await Location.findById(id);
+
       return res.json(locations);
     } catch (err: any) {
       return res.status(500).json({ message: err.message });
@@ -15,32 +16,22 @@ const LocationController = {
 
   async addLocation(req: Request, res: Response) {
     try {
-      const { name, description, coordinates } = req.body;
-      // console.log(name, description, coordinates, "name, description, coordinates");
-      console.log(req.body, "req.body");
+      const { locationName, description, coordinates, photoSrc } = req.body;
 
-      // let location = Location.find();
+      const locaton = await Location.find({ coordinates: coordinates });
 
-      // const locaton = await Location.find({ coordinates: coordinates });
-
-      // // if (locaton.length === 0) {
-      // const newLocation = new Location({
-      //   locationName: name,
-      //   // coordinates: coordinates,
-      //   photoSrc: "",
-      //   description: description,
-      //   // comments: [],
-      //   // rating: {
-      //   //   likes: 0,
-      //   //   dislikes: 0,
-      //   // },
-      // });
-      // newLocation.save(newLocation as any);
-
-      // res.status(200).json(newLocation);
-      // } else {
-      //   res.status(500).json({ message: "Data is present" });
-      // }
+      if (locaton.length === 0) {
+        const newLocation = new Location({
+          locationName: locationName,
+          coordinates: coordinates,
+          photoSrc: photoSrc,
+          description: description,
+        });
+        const result = await newLocation.save(newLocation as any);
+        res.status(200).json(result);
+      } else {
+        res.status(500).json({ message: "Data is present" });
+      }
     } catch (err: any) {
       return res.status(500).json({ message: err.message });
     }
