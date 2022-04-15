73e96f2493449aef2629c50a30b7f8a27022ca63
vasylneshchadimov <neshchadimov_v@ukr.net>
2022-04-15 15:27:47 +0300

    fixed all bags

--- frontend/src/components/BigPopup/index.tsx
+++ frontend/src/components/BigPopup/index.tsx
@@ -1,7 +1,19 @@
 import * as React from 'react';
 import { Drawer } from '@mui/material';
 import Box from '@mui/material/Box';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
+const theme = createTheme({
+  components: {
+    MuiPaper: {
+      styleOverrides: {
+        root: {
+          marginTop: '100px'
+        }
+      }
+    }
+  }
+});
 interface PopupProps {
   isOpen: boolean;
   toggleClose: any; // this function reatern set of useState
@@ -9,21 +21,21 @@ interface PopupProps {
 export default function BigPopup(props: PopupProps) {
   const { isOpen, toggleClose } = props;
 
-  const blackPage: boolean = true;
-
   return (
     <div>
-      <Drawer
-        sx={{ marginTop: 300 }}
-        anchor="left"
-        hideBackdrop={blackPage}
-        open={isOpen}
-        onClose={toggleClose}
-      >
-        <Box sx={{ width: 400, mt: 50 }}>
-          <h2>Location</h2>
-        </Box>
-      </Drawer>
+      <ThemeProvider theme={theme}>
+        <Drawer
+          sx={{ marginTop: 1000 }}
+          anchor="left"
+          hideBackdrop
+          open={isOpen}
+          onClose={toggleClose}
+        >
+          <Box sx={{ width: 400, mt: 50 }}>
+            <h2>Location</h2>
+          </Box>
+        </Drawer>
+      </ThemeProvider>
     </div>
   );
 }
