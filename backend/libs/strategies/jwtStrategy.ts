15d0cf63a207301309dfdd3e277b71693c72788e
Yuriy <41287687+steadyGuy@users.noreply.github.com>
2022-05-03 23:15:26 +0300

    registration done (#24)

---
+++ backend/libs/strategies/jwtStrategy.ts
@@ -0,0 +1,17 @@
+import { Strategy as JWTstrategy, ExtractJwt } from "passport-jwt";
+
+const jwtStrategy = new JWTstrategy(
+  {
+    secretOrKey: process.env.ACCESS_TOKEN_SECRET,
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  },
+  async (token, done) => {
+    try {
+      return done(null, token._id);
+    } catch (error) {
+      done(error);
+    }
+  }
+);
+
+export default jwtStrategy;
