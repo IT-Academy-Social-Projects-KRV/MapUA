03e622caeb13b16b2b5bb1b2069762c13194c1ba
AndriiYozh <yojuks@gmail.com>
2022-05-03 11:26:46 +0300

    Add login layout

---
+++ frontend/src/components/LoginPage/LoginPage.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { FormControl, TextField, Button } from '@mui/material';
+import {
+  BorderForm,
+  RegistrationFormWrapper,
+  StyledSpan,
+  StyledSpanEnd
+} from 'components/LoginPage/styles';
+import {
+  TextFieldWrap,
+  WrapButtonAndText,
+  WrapH1
+} from 'components/ForgotPassword/styles';
+
+function LoginPage() {
+  return (
+    <RegistrationFormWrapper>
+      <BorderForm>
+        <FormControl sx={{ width: '35ch' }}>
+          <WrapH1>
+            <h1>Login</h1>
+          </WrapH1>
+          <TextFieldWrap>
+            <TextField
+              placeholder="Please enter your email"
+              label="Email"
+              autoComplete="current-email"
+            />
+          </TextFieldWrap>
+
+          <TextFieldWrap>
+            <TextField
+              placeholder="Please enter your email"
+              label="Password"
+              type="password"
+              autoComplete="current-password"
+            />
+          </TextFieldWrap>
+
+          <StyledSpanEnd>
+            <span>Forgot the password?</span>
+          </StyledSpanEnd>
+
+          <WrapButtonAndText>
+            <Button variant="contained">Login</Button>
+            <StyledSpan>
+              <span>or</span>
+            </StyledSpan>
+            <Button variant="contained">Sing in with google</Button>
+
+            <StyledSpan>
+              <span>don`t have an account</span>
+            </StyledSpan>
+          </WrapButtonAndText>
+        </FormControl>
+      </BorderForm>
+    </RegistrationFormWrapper>
+  );
+}
+
+export default LoginPage;
