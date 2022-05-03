1a07d03505f9636204509e1fb5897f1fda5aa652
AndriiYozh <yojuks@gmail.com>
2022-05-03 11:27:10 +0300

    Add style to login

---
+++ frontend/src/components/LoginPage/styles.ts
@@ -0,0 +1,29 @@
+import { Box } from '@mui/material';
+import { styled } from '@mui/material/styles';
+
+export const RegistrationFormWrapper = styled(Box)`
+  height: 82vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const BorderForm = styled(RegistrationFormWrapper)`
+  border: 2px solid black;
+  height: 72vh;
+  border-radius: 20px;
+  width: 500px;
+`;
+
+export const StyledSpan = styled(Box)`
+  text-align: end;
+  margin-top: 20px;
+`;
+
+export const StyledSpanOr = styled(Box)`
+  display: block;
+`;
+
+export const StyledSpanEnd = styled(StyledSpan)`
+  margin-top: 15px;
+`;
