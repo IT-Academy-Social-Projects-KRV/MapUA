3a977298e519a86e059ec5ac57b8feab18875275
Kulesh Oleksandr <53787083+6mahdapihka9@users.noreply.github.com>
2022-05-06 00:40:22 +0300

    Feature/map 54/add frontend logic to profile page (#28)
    * created endpoint for user's data fetch, with data plug and type in frontend
    
    * created profile page for testing incoming data, and added it into routes
    
    * registration done
    
    * user model on backend updated regarding the latest our discuss, also updated user data types on front end, commented user data plug cause it's deprecated
    
    * removed unused endpoint
    
    * location model updated: field 'author' added
    
    * user model updated: fields 'locations', 'subscribers' and 'subscription' added
    
    * output data view improved
    
    * resolved conflict that occurred after pulling develop branch
    
    * location model updated
    
    * user model updated, as well as data in mongo db
    
    * removed userToken from model
    
    * userToken changed to fetch by id in UserController.ts,
    changed url in router,
    improved type of incoming data
    
    * on find error resolved, renamed function in profilePage
    
    * removed vscode directory
    
    * removed vscode directory
    
    * changes profile's url in router
    
    * removed profilePage component from develop into Profile folder
    
    * changed path in app router
    
    * Reworked whole user reducer and it's types and actions to fetch proper data by access token and redirect user to '/' in case access token absence. Also tested it - OK
    
    Co-authored-by: Yuriy Soproniuk <sopronukCC@gmail.com>

--- backend/controllers/UserController.ts
+++ backend/controllers/UserController.ts
@@ -2,23 +2,23 @@ import { Response, Request } from "express";
 import User from "../models/UserModel";
 
 const UserController = {
-  async getUserData(req: Request, res: Response) {
+  async getProfile(req: Request, res: Response) {
     try {
-      const _id = req.params._id;
+      const _id = req.user;
 
       const userData = await User.findById(
           _id,
           {
-              email: true,
-              createdAt: true,
-              updatedAt: true,
-              displayName: true,
-              description: true,
-              imageUrl: true,
-              locations: true,
-              subscribers: true,
-              subscriptions: true,
-            });
+            email: true,
+            createdAt: true,
+            updatedAt: true,
+            displayName: true,
+            description: true,
+            imageUrl: true,
+            locations: true,
+            subscribers: true,
+            subscriptions: true,
+          });
 
       if (!userData) {
         return res.status(400).json({ error: "User doesn't exist" });
