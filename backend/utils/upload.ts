2ff31678e633542b02668c2da7c369190aee2e0b
Yuriy Soproniuk <sopronukCC@gmail.com>
2022-05-10 09:23:00 +0300

    aws storage fully implemented

---
+++ backend/utils/upload.ts
@@ -0,0 +1,30 @@
+import multer from "multer";
+import multerS3 from "multer-s3";
+import AWS from "aws-sdk";
+import path from "path";
+
+AWS.config.update({ region: "eu-central-1" });
+
+export const upload = multer({
+  storage: multerS3({
+    s3: new AWS.S3({
+      apiVersion: "latest",
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      accessKeyId: process.env.AWS_SECRET_ACCESS_ID,
+    }),
+    bucket: "mapua-storage",
+    key: (_, file, cb) => {
+      console.log(file);
+      cb(null, `files/${Date.now().toString()}-${file.originalname}`);
+    },
+  }),
+  fileFilter: (_, file, cb) => {
+    const fileTypes = /jpeg|jpg|png|gif/;
+    const mimetype = fileTypes.test(file.mimetype);
+    const extname = fileTypes.test(path.extname(file.originalname));
+    if (mimetype && extname) {
+      return cb(null, true);
+    }
+    cb(new Error("Error: Not allowed mime-type!"));
+  },
+});
