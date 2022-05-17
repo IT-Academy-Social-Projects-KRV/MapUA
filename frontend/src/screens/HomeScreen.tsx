2d8840ec505ceb5d12a39aa37ab4b44a444a62b5
AndriiYozh <yojuks@gmail.com>
2022-05-17 18:06:02 +0300

    Add logic to open Popup

--- frontend/src/screens/HomeScreen.tsx
+++ frontend/src/screens/HomeScreen.tsx
@@ -3,26 +3,47 @@ import { Box } from '@mui/material';
 import Map from 'components/Map/index';
 import BigPopup from 'components/BigPopup/index';
 
+import BigPopupLocation from 'components/design/BigPopupLocation';
 import { locationType } from '../../types';
 
 function HomeScreen() {
-  const [isOpen, setIsopen] = useState(false);
+  const [isOpenLocationPopup, setIsOpenLocationPopup] = useState(false);
+  const [isOpenLocacionForm, setIsOpenLocacionForm] = useState(false);
+
   const [location, setLocation] = useState<locationType | null>(null);
 
   const onOpenBigPopup = (locationData: locationType) => {
     setLocation(locationData);
-    setIsopen(true);
+    setIsOpenLocationPopup(true);
+  };
+
+  const onOpenLocationForm = () => {
+    setIsOpenLocacionForm(true);
   };
 
   return (
     <Box sx={{ flexGrow: 1 }}>
       <BigPopup
-        isOpen={isOpen}
-        toggleClose={() => setIsopen(false)}
+        isOpen={isOpenLocationPopup}
+        toggleClose={() => setIsOpenLocationPopup(false)}
         location={location}
       />
-      <Box sx={{ height: '100%' }} onClick={() => setIsopen(false)}>
-        <Map onOpenBigPopup={onOpenBigPopup} />
+      <BigPopupLocation
+        isOpen={isOpenLocacionForm}
+        toggleClose={() => setIsOpenLocacionForm(false)}
+      >
+        <Box
+          sx={{ width: '300px', height: '600px', backgroundColor: 'tomato' }}
+        />
+      </BigPopupLocation>
+      <Box
+        sx={{ height: '100%' }}
+        onClick={() => setIsOpenLocationPopup(false)}
+      >
+        <Map
+          onOpenBigPopup={onOpenBigPopup}
+          onOpenLocationForm={onOpenLocationForm}
+        />
       </Box>
     </Box>
   );
