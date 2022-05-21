7fcc690812f89c899876fa241d0ce07cad61d233
mazursasha1990 <mazursasha1990@gmail.com>
2022-05-21 18:12:00 +0300

    refactor redux/reducers/index.ts

--- frontend/src/redux/reducers/index.tsx
+++ frontend/src/redux/reducers/index.tsx
@@ -3,14 +3,12 @@ import { userReducer } from './userReducer';
 import { userLoginReducer } from './userAuthReducer';
 import { popupLocationReducer } from './popupLocationReducer';
 import { locationsListReducer } from './locationListReducer';
-// import { filterReducer } from './filtersReducer';
 
 export const rootReducer = combineReducers({
   user: userReducer,
   userAuth: userLoginReducer,
   popupLocation: popupLocationReducer,
   locationList: locationsListReducer
-  // userFilters: filterReducer
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
