d536ca8d31327ba1fadce870c315e3252039849a
nikitius20 <zaika.nikita.01@gmail.com>
2022-05-23 20:04:55 +0300

    fetch user in home screen

--- frontend/src/screens/HomeScreen.tsx
+++ frontend/src/screens/HomeScreen.tsx
@@ -1,11 +1,13 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Box } from '@mui/material';
 import Map from 'components/Map/index';
 import BigPopup from 'components/BigPopup/index';
 
 import BigPopupLocation from 'components/design/BigPopupLocation';
 import CreateLocation from 'components/CreateLocation/CreateLocation';
+import { useTypedSelector } from 'redux/hooks/useTypedSelector';
+import { useTypedDispatch } from 'redux/hooks/useTypedDispatch';
 import { locationType } from '../../types';
 
 function HomeScreen() {
@@ -16,6 +18,9 @@ function HomeScreen() {
   const [location, setLocation] = useState<locationType | null>(null);
   const [coordinate, setCoordinate] = useState<any>([]);
 
+  const { isAuthorized } = useTypedSelector(state => state.userAuth);
+  const { fetchUser } = useTypedDispatch();
+
   const onOpenBigPopup = (locationData: locationType) => {
     setLocation(locationData);
     setIsOpenLocationPopup(true);
@@ -24,6 +29,14 @@ function HomeScreen() {
   const onOpenLocationForm = () => {
     setIsOpenLocacionForm(true);
   };
+
+  useEffect(() => {
+    if (isAuthorized) {
+      const accessToken = localStorage.getItem('accessToken');
+      // @ts-ignore
+      fetchUser(accessToken);
+    }
+  }, [isAuthorized]);
 
   return (
     <Box sx={{ flexGrow: 1 }}>
