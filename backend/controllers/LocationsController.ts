f039fae6829bf3292aca66a27abdf3bf114316f8
Roman Kalishuk <romawhitej@gmail.com>
2022-05-23 12:37:26 +0300

    Fixed searching form

--- backend/controllers/LocationsController.ts
+++ backend/controllers/LocationsController.ts
@@ -7,7 +7,7 @@ const LocationsController = {
     try {
       const center = JSON.parse(req.query.center as string);
       const bounds = JSON.parse(req.query.bounds as string);
-      const name = req.query.name as string;
+      const searchName = req.query.name as string;
       const filters = JSON.parse(req.query.filters as any);
       const height = +(bounds._northEast.lat - bounds._southWest.lat);
       const width = +(bounds._northEast.lng - bounds._southWest.lng);
@@ -28,9 +28,9 @@ const LocationsController = {
         name: l.locationName,
         filters: l.filters
       }));
-      if (name) {
+      if (searchName) {
         locations = locations.filter(l => {
-          return l.name.toLocaleLowerCase().startsWith(name);
+          return l.name.toLowerCase().startsWith(searchName.toLowerCase());
         });
       }
       if (filters.length > 0) {
