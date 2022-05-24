e723c136e57e69d754e6dfe15f8420b17715a824
romawht22 <88610270+romawht22@users.noreply.github.com>
2022-05-24 12:35:26 +0300

    Fix (#83)

--- frontend/src/redux/actions-creators/userAuth.tsx
+++ frontend/src/redux/actions-creators/userAuth.tsx
@@ -11,33 +11,41 @@ const { REACT_APP_API_URI } = process.env;
 // Login
 export const login =
   (email: string, password: string) =>
-    async (dispatch: Dispatch<UserAuthAction>) => {
-      try {
-        dispatch({
-          type: UserAuthActionTypes.USER_LOGIN_REQUEST
-        });
+  async (dispatch: Dispatch<UserAuthAction>) => {
+    try {
+      dispatch({
+        type: UserAuthActionTypes.USER_LOGIN_REQUEST
+      });
 
-        const response = await axios.post(`${REACT_APP_API_URI}signin`, {
+      const response = await axios.post(
+        `${REACT_APP_API_URI}signin`,
+        {
           email,
           password
-        });
+        },
+        {
+          headers: {
+            'Accept-Language': 'ua'
+          }
+        }
+      );
 
-        dispatch({
-          type: UserAuthActionTypes.USER_LOGIN_SUCCESS,
-          payload: response.data
-        });
+      dispatch({
+        type: UserAuthActionTypes.USER_LOGIN_SUCCESS,
+        payload: response.data
+      });
 
-        localStorage.setItem('accessToken', response.data.token);
-      } catch (error: any) {
-        dispatch({
-          type: UserAuthActionTypes.USER_LOGIN_FAIL,
-          payload:
-            error.response && error.response.data.massage
-              ? error.response.data.message
-              : error.message
-        });
-      }
-    };
+      localStorage.setItem('accessToken', response.data.token);
+    } catch (error: any) {
+      dispatch({
+        type: UserAuthActionTypes.USER_LOGIN_FAIL,
+        payload:
+          error.response && error.response.data.massage
+            ? error.response.data.message
+            : error.message
+      });
+    }
+  };
 
 export const loginOAuth =
   (token: string, id: string) => async (dispatch: Dispatch<UserAuthAction>) => {
