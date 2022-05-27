3fd8f7f601de56e918f78374fda9a4e45fe8790a
vasylneshchadimov <neshchadimov_v@ukr.net>
2022-05-28 02:14:10 +0300

    add language localization

--- frontend/src/components/Page_404/Page404.tsx
+++ frontend/src/components/Page_404/Page404.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Box, Link } from '@mui/material';
 import { Link as RouterLink } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 import { StyledForPage404 } from './style';
 
 // eslint-disable-next-line arrow-body-style
 const Page404 = () => {
+  const { t } = useTranslation();
   return (
     <StyledForPage404>
       <Box sx={{ marginTop: '20px' }}>
         <Link underline="none" component={RouterLink} to="/">
-          Return to main page
+          {t('page404.Return to main page')}
         </Link>
       </Box>
     </StyledForPage404>
