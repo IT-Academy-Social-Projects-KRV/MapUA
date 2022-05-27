aecc682fba2c838cc2c0e6863d0a3476ed27c626
mazursasha1990 <85229023+mazursasha1990@users.noreply.github.com>
2022-05-27 11:10:19 +0300

    Fix/refactor localization for frontend (#94)
    * Add basic structure for localization
    
    * Merge develop
    
    * Add localstorage as default detection language
    
    * Add translate to all current  files
    
    * Create translation to all components
    
    * First commit
    
    * Add toggle buttons for choosing language an refactor i18n.tsx
    
    * Add changes to i18n.tsx
    
    * fix i18next settings

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,7 +4,7 @@ import {
   ThemeProvider as CoreThemeProvider
 } from '@mui/material/styles';
 import 'leaflet/dist/leaflet.css';
-import React from 'react';
+import React, { Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
 import theme from 'theme';
 
@@ -14,13 +14,12 @@ import icon from 'leaflet/dist/images/marker-icon.png';
 import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 
 import { Provider } from 'react-redux';
-// import { I18nextProvider } from 'react-i18next';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import { store } from './redux/store';
 
-// import i18n from './i18n';
+import './i18n';
 
 // fix for popup icon
 const DefaultIcon = L.icon({
@@ -33,18 +32,18 @@ const rootElement = document.getElementById('root');
 const root = createRoot(rootElement!);
 
 root.render(
-  <React.StrictMode>
-    {/* <I18nextProvider i18n={i18n}> */}
-    <ThemeProvider theme={theme}>
-      <CoreThemeProvider theme={theme}>
-        <CssBaseline />
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </CoreThemeProvider>
-    </ThemeProvider>
-    {/* </I18nextProvider> */}
-  </React.StrictMode>
+  <Suspense fallback={<div>Loading...</div>}>
+    <React.StrictMode>
+      <ThemeProvider theme={theme}>
+        <CoreThemeProvider theme={theme}>
+          <CssBaseline />
+          <Provider store={store}>
+            <App />
+          </Provider>
+        </CoreThemeProvider>
+      </ThemeProvider>
+    </React.StrictMode>
+  </Suspense>
 );
 
 reportWebVitals();
