883d83085361b441f33c502948d2c1b00ad6706f
Roman Kalishuk <romawhitej@gmail.com>
2022-05-30 15:34:39 +0300

    Added proper styles

--- frontend/src/components/CreateLocation/CreateLocation.tsx
+++ frontend/src/components/CreateLocation/CreateLocation.tsx
@@ -65,7 +65,7 @@ const CreateLocation = ({ coordinate }: Props) => {
         ref.current.value = '';
       }
     } catch (error) {
-      console.log(error);
+      console.error(error);
     }
   };
 
