b5d1ec6f45ba5e37ff6ff4b2c839f1dfa9468592
Andrii <yojuks@gmail.com>
2022-05-31 13:04:24 +0300

    Create EditGroup component

---
+++ frontend/src/components/Profile/EditGroup/EditGroup.tsx
@@ -0,0 +1,39 @@
+import { Box, Typography } from '@mui/material';
+import React, { FC } from 'react';
+import { useTypedSelector } from 'redux/hooks/useTypedSelector';
+import userImageNotFound from '../../../static/image-not-found.jpg';
+import { EditButton, ProfileAvatar } from '../styles';
+import { useTranslation } from 'react-i18next';
+
+type Props = {
+  displayName: string;
+  editData: any;
+};
+
+export const EditGroup: FC<Props> = ({ displayName, editData }) => {
+  const { t } = useTranslation();
+  const userAvatar = useTypedSelector(state => state.user);
+
+  return (
+    <Box>
+      <ProfileAvatar
+        aria-label="avatar"
+        src={userAvatar.data.imageUrl || userImageNotFound}
+      />
+      <Typography sx={{ mt: '3vh' }} variant="h5" component="h4" align="center">
+        {displayName === undefined
+          ? `${t('profile.profilePage.yourName')}`
+          : displayName}
+      </Typography>
+
+      <EditButton
+        sx={{ mt: '2vh' }}
+        size="large"
+        variant="contained"
+        onClick={editData}
+      >
+        {t('profile.profilePage.editProfile')}
+      </EditButton>
+    </Box>
+  );
+};
