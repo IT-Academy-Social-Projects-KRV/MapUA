c1984d3afcba9280fd9b058867b7306906347f59
Andrii <yojuks@gmail.com>
2022-05-31 13:02:56 +0300

    Create Controllers component

---
+++ frontend/src/components/Registration/Controllers/Controllers.tsx
@@ -0,0 +1,91 @@
+import { Box, IconButton, InputAdornment, TextField } from '@mui/material';
+import React, { FC, MouseEventHandler } from 'react';
+import { Controller, useForm, useFormState } from 'react-hook-form';
+import { useTranslation } from 'react-i18next';
+import { emailValidation, passwordValidation } from 'utils/validation';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import Visibility from '@mui/icons-material/Visibility';
+
+type Props = {
+  handleClickShowPassword: MouseEventHandler<HTMLButtonElement>;
+  handleMouseDownPassword: MouseEventHandler<HTMLButtonElement>;
+  showPassword: boolean;
+};
+
+type SignUp = {
+  email: string;
+  password: string;
+};
+
+export const Controllers: FC<Props> = ({
+  handleClickShowPassword,
+  handleMouseDownPassword,
+  showPassword
+}) => {
+  const { t } = useTranslation();
+
+  const { control } = useForm<SignUp>({
+    mode: 'onBlur'
+  });
+
+  const { errors } = useFormState({
+    control
+  });
+
+  return (
+    <>
+      <Controller
+        control={control}
+        name="email"
+        rules={emailValidation}
+        render={({ field }) => (
+          <TextField
+            placeholder={t('common.enterYourEmail')}
+            label="Email"
+            autoComplete="current-email"
+            fullWidth
+            onChange={e => field.onChange(e)}
+            onBlur={field.onBlur}
+            defaultValue={field.value}
+            type="text"
+            error={!!errors.email?.message}
+            helperText={errors.email?.message}
+          />
+        )}
+      />
+      <Box sx={{ my: 4 }}>
+        <Controller
+          control={control}
+          name="password"
+          rules={passwordValidation}
+          render={({ field }) => (
+            <TextField
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                )
+              }}
+              type={showPassword ? 'text' : 'password'}
+              placeholder={t('common.enterPassword')}
+              label={t('common.password')}
+              autoComplete="current-password"
+              fullWidth
+              onChange={e => field.onChange(e)}
+              onBlur={field.onBlur}
+              defaultValue={field.value}
+              error={!!errors.password?.message}
+              helperText={errors.password?.message}
+            />
+          )}
+        />
+      </Box>
+    </>
+  );
+};
