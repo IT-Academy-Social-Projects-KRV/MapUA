99522c818555d08f28f626d510e6293aa4b0c715
Andrii Yozh <39120801+Yojuks@users.noreply.github.com>
2022-06-03 11:15:01 +0300

    Fix/profile fix (#123)
    * Change ProfilePage
    
    * Delete EditGroup
    
    * Delete SaveGroup
    
    * fixed snackbar invocation in profilePage
    
    * Change branch name

--- frontend/src/components/Profile/ProfilePage.tsx
+++ frontend/src/components/Profile/ProfilePage.tsx
@@ -1,33 +1,51 @@
 import React, { useEffect, useState, useRef } from 'react';
-import { Typography, Button, Box, Snackbar, Alert } from '@mui/material';
-import { useForm, SubmitHandler, useFormState } from 'react-hook-form';
+import {
+  Typography,
+  Button,
+  Box,
+  TextField,
+  Snackbar,
+  Alert
+} from '@mui/material';
+import {
+  useForm,
+  SubmitHandler,
+  Controller,
+  useFormState
+} from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { EditProfileSchema } from 'utils/validation';
 import { useTranslation } from 'react-i18next';
 import { useTypedSelector } from 'redux/hooks/useTypedSelector';
 import { useTypedDispatch } from 'redux/hooks/useTypedDispatch';
+import UploadInputProfilePage from 'components/design/UploadInputProfilePage';
+import userImageNotFound from '../../static/user-image-not-found.png';
 import {
+  ProfileAvatar,
+  SaveButton,
+  CancelButton,
   ProfileContentWrapper,
   ProfileFormWrapper,
   ProfileUserWrapper,
+  SaveBox,
+  UploadBox,
+  EditButton,
   TypographyDate
 } from './styles';
 import BasicTabs from './BasicTabs';
-import { UserForm } from '../../../types';
-import { SaveGroup } from './SaveGroup/SaveGroup';
-import { EditGroup } from './EditGroup/EditGroup';
 import useDebounce from '../../utils/useDebounce';
+import { UserForm } from '../../../types';
 
 export default function ProfilePage() {
-  const { t } = useTranslation();
   const isMounted = useRef(false);
+  const { t } = useTranslation();
 
   const { updateUserData, deleteUserData, deletePrivateUserData, logout } =
     useTypedDispatch();
   const {
     success: updateSuccess,
     error: updateError,
-    data: { _id: id, displayName, description }
+    data: { _id: id, displayName, description, imageUrl: userAvatar }
   } = useTypedSelector(state => state.userData);
   const { email, createdAt, updatedAt } = useTypedSelector(
     state => state.privateUserData.data
@@ -38,16 +56,14 @@ export default function ProfilePage() {
   const [userImage, setUserImage] = useState<File | null>();
   const [errorMessage, setErrorMessage] = useState('');
 
-  const { handleSubmit, control } = useForm<UserForm>({
+  const { handleSubmit, control, register } = useForm<UserForm>({
     mode: 'onBlur',
     defaultValues: { displayName, description },
     resolver: yupResolver(EditProfileSchema)
   });
-
   const { errors } = useFormState({
     control
   });
-
   useEffect(() => {
     if (isMountedDebounced) {
       if (updateError) {
@@ -73,7 +89,6 @@ export default function ProfilePage() {
       isMounted.current = true;
     }
   }, [updateSuccess]);
-
   const onSubmit: SubmitHandler<UserForm> = async data => {
     const formData = new FormData();
     if (userImage) {
@@ -82,7 +97,6 @@ export default function ProfilePage() {
     formData.append('id', id);
     formData.append('displayName', data.displayName);
     formData.append('description', data.description);
-
     updateUserData(formData);
   };
   const editData = () => {
@@ -101,7 +115,6 @@ export default function ProfilePage() {
     setErrorMessage('');
     setSuccessMessage(false);
   };
-
   return (
     <Box component="form" onSubmit={handleSubmit(onSubmit)}>
       <ProfileFormWrapper>
@@ -117,7 +130,6 @@ export default function ProfilePage() {
               {t('profile.profilePage.dataSuccessChanged')}
             </Alert>
           </Snackbar>
-
           <Snackbar
             anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
             sx={{ zIndex: 10000 }}
@@ -131,16 +143,74 @@ export default function ProfilePage() {
           </Snackbar>
 
           {showEditPanel ? (
-            <SaveGroup
-              onSubmit={onSubmit}
-              displayName={displayName}
-              description={displayName}
-              closeEditData={closeEditData}
-              setUserImage={setUserImage}
-              userImage={userImage}
-            />
+            <Box>
+              <UploadBox>
+                <ProfileAvatar
+                  aria-label="avatar"
+                  src={
+                    (userImage && URL.createObjectURL(userImage)) || userAvatar
+                  }
+                />
+                <UploadInputProfilePage
+                  setUserImage={setUserImage}
+                  register={register}
+                />
+              </UploadBox>
+              <Controller
+                control={control}
+                name="displayName"
+                render={({ field }) => (
+                  <TextField
+                    placeholder={t('profile.profilePage.enterName')}
+                    label={t('profile.profilePage.name')}
+                    fullWidth
+                    onChange={field.onChange}
+                    onBlur={field.onBlur}
+                    defaultValue={field.value}
+                    type="text"
+                    error={!!errors.displayName?.message}
+                    helperText={t(
+                      !errors.displayName
+                        ? ''
+                        : String(errors.displayName.message)
+                    )}
+                  />
+                )}
+              />
+              <SaveBox>
+                <SaveButton size="large" variant="contained" type="submit">
+                  {t('profile.profilePage.save')}
+                </SaveButton>
+                <CancelButton
+                  size="large"
+                  variant="contained"
+                  onClick={closeEditData}
+                >
+                  {t('profile.profilePage.cancel')}
+                </CancelButton>
+              </SaveBox>
+            </Box>
           ) : (
-            <EditGroup displayName={displayName} editData={editData} />
+            <UploadBox>
+              <ProfileAvatar
+                aria-label="avatar"
+                src={userAvatar || userImageNotFound}
+              />
+              <Typography
+                sx={{ mt: '3vh' }}
+                variant="h5"
+                component="h4"
+                align="center"
+              >
+                {displayName === undefined
+                  ? `${t('profile.profilePage.yourName')}`
+                  : displayName}
+              </Typography>
+
+              <EditButton size="large" variant="contained" onClick={editData}>
+                {t('profile.profilePage.editProfile')}
+              </EditButton>
+            </UploadBox>
           )}
 
           <TypographyDate variant="h6">
@@ -151,7 +221,6 @@ export default function ProfilePage() {
             {t('profile.profilePage.updateDate')}{' '}
             {new Date(updatedAt).toLocaleDateString('en-GB')}
           </TypographyDate>
-
           <Typography variant="h6" component="h6" align="center">
             {email}
           </Typography>
