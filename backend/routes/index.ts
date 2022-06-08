b05109ba5281dec90032f78477ee75e15f92e428
Roman Kalishuk <romawhitej@gmail.com>
2022-06-08 16:58:52 +0300

    Added getOtherUser Endpoin

--- backend/routes/index.ts
+++ backend/routes/index.ts
@@ -128,5 +128,6 @@ router.put(
   passport.authenticate('jwt', { session: false }),
   UserController.toggleVisited
 );
+router.get('/profile/:id', UserController.getOtherUserProfile);
 
 export default router;
