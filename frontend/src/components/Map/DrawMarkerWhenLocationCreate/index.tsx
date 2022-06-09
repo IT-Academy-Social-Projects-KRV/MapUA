314d99996e7f7c8751874392568cca25b2608131
vasylneshchadimov <neshchadimov_v@ukr.net>
2022-06-09 20:40:12 +0300

    fix marker position

--- frontend/src/components/Map/DrawMarkerWhenLocationCreate/index.tsx
+++ frontend/src/components/Map/DrawMarkerWhenLocationCreate/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Marker } from 'react-leaflet';
+import L from 'leaflet';
 import { latlngType } from '../../../../types';
+import icon from '../../../static/leaf-green.png';
+
+import shadow from '../../../static/leaf-shadow.png';
 
 type Props = {
   coordinate: latlngType;
@@ -9,7 +13,17 @@ function DrawMarkerCreateLocation({ coordinate }: Props) {
   const { lat, lng } = coordinate;
   return (
     <div>
-      <Marker position={[lat, lng]} />
+      <Marker
+        position={[lat, lng]}
+        icon={L.icon({
+          iconUrl: icon,
+          shadowUrl: shadow,
+          iconSize: [38, 95],
+          shadowSize: [50, 64],
+          iconAnchor: [30, 94],
+          shadowAnchor: [4, 62]
+        })}
+      />
     </div>
   );
 }
