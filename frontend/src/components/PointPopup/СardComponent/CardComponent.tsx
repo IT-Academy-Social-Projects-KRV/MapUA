18b310b0e5ae3738dabcbb1a600eba1e7b8166cc
Andrii Yozh <yojuks@gmail.com>
2022-06-09 18:09:59 +0300

    Chang Card Component

--- frontend/src/components/PointPopup/СardComponent/CardComponent.tsx
+++ frontend/src/components/PointPopup/СardComponent/CardComponent.tsx
@@ -3,19 +3,26 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import React, { FC, MouseEventHandler } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useTypedSelector } from 'redux/hooks/useTypedSelector';
-import userImageNotFound from '../../../static/image-not-found.jpg';
+import { Controller } from 'react-hook-form';
 import { StyledCardComponentBox } from '../../design/StyledCardComponentBox';
+import userImageNotFound from '../../../static/image-not-found.jpg';
 
 type Props = {
   description: string;
   handleExpandClick: MouseEventHandler<HTMLButtonElement>;
   expanded: boolean;
+  showEditPanel: boolean;
+  control: any;
+  errors: any;
 };
 
 export const CardComponent: FC<Props> = ({
   description,
   handleExpandClick,
-  expanded
+  expanded,
+  showEditPanel,
+  control,
+  errors
 }) => {
   const { createdAt, author } = useTypedSelector(
     state => state.popupLocation.data
@@ -37,14 +44,37 @@ export const CardComponent: FC<Props> = ({
 
           <Typography>{createdAt.toLocaleDateString()}</Typography>
         </StyledCardComponentBox>
-        <TextField
-          margin="normal"
-          disabled
-          multiline
-          fullWidth
-          minRows={2}
-          defaultValue={description}
-        />
+
+        {showEditPanel ? (
+          <Controller
+            control={control}
+            name="description"
+            render={({ field }) => (
+              <TextField
+                placeholder={t('profile.profilePage.enterName')}
+                label={t('profile.profilePage.name')}
+                fullWidth
+                onChange={field.onChange}
+                onBlur={field.onBlur}
+                defaultValue={description}
+                type="text"
+                error={!!errors.description?.message}
+                helperText={t(
+                  !errors.description ? '' : String(errors.description.message)
+                )}
+              />
+            )}
+          />
+        ) : (
+          <TextField
+            margin="normal"
+            disabled
+            multiline
+            fullWidth
+            minRows={2}
+            defaultValue={description}
+          />
+        )}
       </Box>
       <IconButton
         onClick={handleExpandClick}
