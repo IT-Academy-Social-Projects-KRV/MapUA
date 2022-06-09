2de35d33718c783f431d08cc8ceeac1b6c65cb72
Oleksandr Bozhko <obozhko@softserveinc.com>
2022-06-09 20:45:36 +0300

    add function toogleUserSubscription to action creator UserData

--- frontend/src/components/PersonProfile/PersonProfilePage.tsx
+++ frontend/src/components/PersonProfile/PersonProfilePage.tsx
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-unused-vars
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Typography } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import { useTypedSelector } from 'redux/hooks/useTypedSelector';
@@ -17,22 +17,40 @@ import BasicTabs from './BasicTabs';
 
 export default function PersonProfilePage() {
   const { t } = useTranslation();
+  // const [isSubscribed, setIsSubscribed] = useState(false);
+  const {
+    loading: userLoading,
+    data: { _id: otherUserId, displayName, imageUrl: userAvatar }
+  } = useTypedSelector(state => state.otherUserData);
+  const {
+    data: { _id: userId, subscriptions }
+  } = useTypedSelector(state => state.userData);
 
-  const { loading: userLoading } = useTypedSelector(
-    state => state.otherUserData
-  );
+  const { fetchOtherUserData, toogleUserSubscription } = useTypedDispatch();
+
+  const isSubscribed = subscriptions.includes(otherUserId);
+  // console.log('otherId: ', otherUserId);
+  // console.log('userId: ', userId);
+  // console.log('subscriptions: ', subscriptions.includes(otherUserId));
+  console.log('subscriptions: ', subscriptions);
 
   const params = useParams();
-  const { fetchOtherUserData } = useTypedDispatch();
   useEffect(() => {
     fetchOtherUserData(params?.id || '');
   }, [params.id]);
-  const {
-    data: { displayName, imageUrl: userAvatar }
-  } = useTypedSelector(state => state.otherUserData);
+
   if (userLoading) {
     return <h1>{t('profile.profile.loading')}</h1>;
   }
+
+  const handleSubscription = () => {
+    toogleUserSubscription(
+      otherUserId,
+      userId,
+      t('profile.profilePage.profilePageUpdatedSuccessfully')
+    );
+  };
+
   return (
     <ProfileFormWrapper>
       <ProfileContentWrapper sx={{ height: 'auto' }}>
@@ -45,8 +63,14 @@ export default function PersonProfilePage() {
             ? `${t('profile.profilePage.yourName')}`
             : displayName}
         </Typography>
-        <SubsrcibeButton size="large" variant="contained" onClick={() => null}>
-          {t('profile.profilePage.subscribe')}
+        <SubsrcibeButton
+          size="large"
+          variant="contained"
+          onClick={handleSubscription}
+        >
+          {!isSubscribed
+            ? t('profile.profilePage.subscribe')
+            : t('profile.profilePage.unsubscribe')}
         </SubsrcibeButton>
       </ProfileContentWrapper>
 
--- frontend/src/redux/actions-creators/userData.tsx
+++ frontend/src/redux/actions-creators/userData.tsx
@@ -48,9 +48,57 @@ export const updateUserData =
           }
         }
       );
+      console.log('response: ', response.data);
       dispatch({
         type: UserDataActionTypes.UPDATE_USER_DATA_SUCCESS,
         payload: response.data
+      });
+      dispatch({
+        type: SnackbarActionsType.SET_SUCCESS,
+        payload: notification
+      });
+    } catch (error: any) {
+      dispatch({
+        type: UserDataActionTypes.UPDATE_USER_DATA_ERROR,
+        payload: error.response.data?.error || 'lost network'
+      });
+      dispatch({
+        type: SnackbarActionsType.SET_ERROR,
+        payload: error.response.data?.error || 'lost network'
+      });
+      throw new Error(error);
+    }
+  };
+
+export const toogleUserSubscription =
+  (otherUserId: string, userId: string, notification: string) =>
+  async (dispatch: Dispatch<UserDataAction | SnackbarActions>) => {
+    try {
+      dispatch({ type: UserDataActionTypes.UPDATE_USER_DATA_LOADING });
+      // const response = await axios().patch(
+      //   `${process.env.REACT_APP_API_URI}toogleSubscribe`,
+      //   userId, otherUserId
+      // );
+      const response = {
+        _id: userId,
+        displayName: 'Okeany',
+        description: 'Descr  Okeany',
+        imageUrl:
+          'https://mapua-storage.s3.eu-central-1.amazonaws.com/files/1654365858151-1653380005599.jpg',
+        subscribers: [],
+        // subscriptions: [],
+        subscriptions: [
+          '628231392c5e18fa0824a19c',
+          '6297b935a1c2a41812bb02ca',
+          '6297b935a1c2a41812bb0222'
+        ],
+        favorite: [],
+        visited: [],
+        personalLocations: []
+      };
+      dispatch({
+        type: UserDataActionTypes.UPDATE_USER_DATA_SUCCESS,
+        payload: response
       });
       dispatch({
         type: SnackbarActionsType.SET_SUCCESS,
