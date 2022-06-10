f238634fcf7cb1bd25384196667cc0f2f3f7c3ce
Vasyl <neshchadimov_v@ukr.net>
2022-06-10 18:19:01 +0300

    Map 170/create clickable location marker when you add location (#137)
    * Create_clickable_location_marker_when_you_add_location
    
    * version 0.01
    
    * fix marker position
    
    * new icon marker

--- frontend/src/components/Map/index.tsx
+++ frontend/src/components/Map/index.tsx
@@ -12,8 +12,11 @@ import { useTypedDispatch } from 'redux/hooks/useTypedDispatch';
 import L from 'leaflet';
 import Locations from './Locations/Locations';
 import MyZoomComponent from './ZoomComponent';
+
 import { StyledMapContainer } from '../design/StyledMapContainer';
 import { StyledAddLocationButton } from '../design/StyledAddLocationButton';
+import { latlngType } from '../../../types';
+import DrawMarkerCreateLocation from './DrawMarkerWhenLocationCreate';
 
 interface Props {
   onOpenBigPopup: Function;
@@ -22,6 +25,7 @@ interface Props {
   isOpen: boolean;
   toggleIsAddLocation: Function;
   isAddLocationActive: boolean;
+  coordinate: latlngType;
 }
 
 function Map({
@@ -30,7 +34,8 @@ function Map({
   isOpen,
   toggleIsAddLocation,
   isAddLocationActive,
-  onOpenBigPopup
+  onOpenBigPopup,
+  coordinate
 }: Props) {
   const { t } = useTranslation();
   const { isAuthorized } = useTypedSelector(
@@ -93,6 +98,7 @@ function Map({
               : `${t('map.addLocation')}`}
           </StyledAddLocationButton>
         )}
+        {isOpen && <DrawMarkerCreateLocation coordinate={coordinate} />}
       </StyledMapContainer>
     </StyledMapWrapper>
   );
