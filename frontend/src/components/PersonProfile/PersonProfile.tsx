5f023b007dfa22a9cab03db4a726da8dfebd4760
Yurii Stupnytskiy <wedmezin@gmail.com>
2022-06-13 13:33:24 +0300

    fix comment id params

--- frontend/src/components/PersonProfile/PersonProfile.tsx
+++ frontend/src/components/PersonProfile/PersonProfile.tsx
@@ -5,7 +5,6 @@ import PersonProfilePage from './PersonProfilePage';
 
 function PersonProfile() {
   const { error: userError } = useTypedSelector(state => state.otherUserData);
-  
   if (userError) {
     return <h1>{userError && <h6>{userError}</h6>}</h1>;
   }
