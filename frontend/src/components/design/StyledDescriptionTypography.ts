e7866ac8d336bb2174f347c63f78061ab836ea07
Andrii Yozh <yojuks@gmail.com>
2022-06-20 14:55:52 +0300

    Create StyledDescriptionTypography

---
+++ frontend/src/components/design/StyledDescriptionTypography.ts
@@ -0,0 +1,11 @@
+import { Typography } from '@mui/material';
+import { styled } from '@mui/material/styles';
+
+export const StyledDescriptionTypography = styled(Typography)(() => ({
+  width: '100%',
+  minHeight: 100,
+  border: '1px solid grey',
+  borderRadius: 15,
+  padding: 10,
+  marginTop: 25
+}));
