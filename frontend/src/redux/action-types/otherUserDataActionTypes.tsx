07b63b61e203da23caef722b4191c235520e3d1e
Andrii Yozh <yojuks@gmail.com>
2022-07-08 12:32:58 +0300

    Change action types

--- frontend/src/redux/action-types/otherUserDataActionTypes.tsx
+++ frontend/src/redux/action-types/otherUserDataActionTypes.tsx
@@ -7,9 +7,12 @@ export enum OtherUserDataActionTypes {
   FETCH_OTHER_USER_DATA_ERROR = 'FETCH_OTHER_USER_DATA_ERROR',
   UPDATE_OTHER_USER_DATA_LOADING = 'UPDATE_OTHER_USER_DATA_LOADING',
   UPDATE_OTHER_USER_DATA_SUCCESS = 'UPDATE_OTHER_USER_DATA_SUCCESS',
-  UPDATE_OTHER_USER_DATA_ERROR = 'UPDATE_OTHER_USER_DATA_ERROR'
-}
+  UPDATE_OTHER_USER_DATA_ERROR = 'UPDATE_OTHER_USER_DATA_ERROR',
 
+  UPDATE_USER_DATA_AND_BAN_LOADING = 'UPDATE_USER_DATA_AND_BAN_LOADING',
+  UPDATE_USER_DATA_AND_BAN_SUCCESS = 'UPDATE_USER_DATA_AND_BAN_SUCCESS',
+  UPDATE_USER_DATA_AND_BAN_ERROR = 'UPDATE_USER_DATA_AND_BAN_ERROR'
+}
 interface FetchOtherUserLoadingAction {
   type: OtherUserDataActionTypes.FETCH_OTHER_USER_DATA_LOADING;
 }
@@ -34,10 +37,25 @@ interface UpdateOtherUserErrorAction {
   payload: string;
 }
 
+interface UpdateUserDataAndBanLoadingAction {
+  type: OtherUserDataActionTypes.UPDATE_USER_DATA_AND_BAN_LOADING;
+}
+interface UpdateUserDataAndBanSuccessAction {
+  type: OtherUserDataActionTypes.UPDATE_USER_DATA_AND_BAN_SUCCESS;
+  payload: UserDataType;
+}
+interface UpdateUserDataAndBanErrorAction {
+  type: OtherUserDataActionTypes.UPDATE_USER_DATA_AND_BAN_ERROR;
+  payload: string;
+}
+
 export type OtherUserDataAction =
   | FetchOtherUserLoadingAction
   | FetchOtherUserSuccessAction
   | FetchOtherUserErrorAction
   | UpdateOtherUserLoadingAction
   | UpdateOtherUserSuccessAction
-  | UpdateOtherUserErrorAction;
+  | UpdateOtherUserErrorAction
+  | UpdateUserDataAndBanLoadingAction
+  | UpdateUserDataAndBanSuccessAction
+  | UpdateUserDataAndBanErrorAction;
