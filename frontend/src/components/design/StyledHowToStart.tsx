729ba553ec9cdf3d2f33b27aa625b28b01e867ec
Yurii Stupnytskiy <wedmezin@gmail.com>
2022-07-11 14:34:12 +0300

    add component

---
+++ frontend/src/components/design/StyledHowToStart.tsx
@@ -0,0 +1,14 @@
+import { Box, Button } from '@mui/material';
+import { styled } from '@mui/material/styles';
+
+export const StyledMapWrapper = styled(Box)`
+  height: 100%;
+  width: 100%;
+`;
+export const Text = styled(Box)`
+  margin: 20px 0 0 0;
+`;
+export const ButtonTitle = styled(Button)`
+  text-transform: none;
+  font-size: 16px;
+`;
