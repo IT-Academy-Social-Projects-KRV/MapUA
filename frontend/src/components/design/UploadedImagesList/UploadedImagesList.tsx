39cd79f4ffb7a1737b4254a1e5e6f7d64d3a950b
6mahdapihka9 <shnyger@gmail.com>
2022-07-12 20:34:01 +0300

    added warning that at least one image must be

--- frontend/src/components/design/UploadedImagesList/UploadedImagesList.tsx
+++ frontend/src/components/design/UploadedImagesList/UploadedImagesList.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import { Box } from '@mui/material';
+import React, { useState, useEffect } from 'react';
+import { Box, Typography } from '@mui/material';
 import { v4 } from 'uuid';
 import { styled } from '@mui/material/styles';
+import { useTranslation } from 'react-i18next';
 import UploadedImageBox from './UploadedImageBox/UploadedImageBox';
 
 type UploadedImagesListProps = {
@@ -16,17 +17,42 @@ const UploadedImagesListWrapper = styled(Box)`
 `;
 
 const UploadedImagesList = ({ files, setFiles }: UploadedImagesListProps) => {
+  const [isErrorVisible, setIsErrorVisible] = useState<boolean>(false);
+  const { t } = useTranslation();
+
+  useEffect(() => {
+    if (files.length > 1) {
+      setIsErrorVisible(false);
+    } else {
+      setTimeout(() => setIsErrorVisible(false), 3000);
+    }
+  }, [files]);
+
   const handleRemoveFile = (file: File) => {
-    setFiles((prev: File[]) => [
-      ...prev.filter((f: File) => f.name !== file.name)
-    ]);
+    setFiles((prev: File[]) => {
+      if (prev.length > 1) {
+        return [...prev.filter((f: File) => f.name !== file.name)];
+      }
+      setIsErrorVisible(true);
+      return [...prev];
+    });
   };
+
   return (
-    <UploadedImagesListWrapper>
-      {files.map(f => (
-        <UploadedImageBox key={v4()} file={f} onRemoveFile={handleRemoveFile} />
-      ))}
-    </UploadedImagesListWrapper>
+    <>
+      <UploadedImagesListWrapper>
+        {files.map(f => (
+          <UploadedImageBox
+            key={v4()}
+            file={f}
+            onRemoveFile={handleRemoveFile}
+          />
+        ))}
+      </UploadedImagesListWrapper>
+      <Typography color="red">
+        {isErrorVisible ? t('pointPopUp.mustBeAtLeastOneImage') : ''}
+      </Typography>
+    </>
   );
 };
 
