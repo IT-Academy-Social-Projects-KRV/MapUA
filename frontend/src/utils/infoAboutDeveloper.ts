e918382b217d880359cf91e21e12f372024f9a27
Vlad Ananchenko <ananchenko346@gmail.com>
2022-07-14 14:23:55 +0300

    4 photo

--- frontend/src/utils/infoAboutDeveloper.ts
+++ frontend/src/utils/infoAboutDeveloper.ts
@@ -1,9 +1,9 @@
 export const infoAboutDeveloper = [
   {
-    photo:
-      'https://media2.nekropole.info/2013/05/375px-Vasco_da_Gama_-_1838.png',
+    photo: '/photo/Bozhko.jpg',
     fullName: 'Oleksandr Bozhko',
-    linkedIn: 'https://www.linkedin.com'
+    linkedIn:
+      'https://www.linkedin.com/authwall?trk=bf&trkInfo=AQFppOJrt7Pr4QAAAYH8OXGA7XzIP5Jy2GuSAmfwjmor0331YruL3mB53Ll-OD8_oJxjGCmxwMxadIdJDhHqkAgn_aL7vDci1FguIRSUGWHhIb9-I3ecqfIQnhVHuZw58WAFvfI=&original_referer=&sessionRedirect=https%3A%2F%2Fwww.linkedin.com%2Fin%2Foleksandr-bozhko-1509a7115%2F'
   },
   {
     photo:
@@ -42,16 +42,16 @@ export const infoAboutDeveloper = [
     linkedIn: 'https://www.linkedin.com'
   },
   {
-    photo:
-      'https://media2.nekropole.info/2013/05/375px-Vasco_da_Gama_-_1838.png',
+    photo: '/photo/Neshchadimov.jpg',
     fullName: 'Vasyl Neshchadimov',
-    linkedIn: 'https://www.linkedin.com'
+    linkedIn:
+      'https://www.linkedin.com/authwall?trk=bf&trkInfo=AQHTaR0Bf4Bf8QAAAYH8OjTQktwklFuFoWyd7HM0k4GRhvC4bRWcWbiPOGzVXLXTTYA5oAbesKpZp7rtsjpcFsVWT4zkh71d3F0iKQNiiX2A0HhooZgCDagxVEdjJvDkNRaUjE8=&original_referer=&sessionRedirect=https%3A%2F%2Fwww.linkedin.com%2Fin%2F%25D0%25B2%25D0%25B0%25D1%2581%25D0%25B8%25D0%25BB%25D1%258C-%25D0%25BD%25D0%25B5%25D1%2589%25D0%25B0%25D0%25B4%25D1%2596%25D0%25BC%25D0%25BE%25D0%25B2-77a6aa207%2F'
   },
   {
-    photo:
-      'https://media2.nekropole.info/2013/05/375px-Vasco_da_Gama_-_1838.png',
+    photo: '/photo/Stupnytskyi.jpg',
     fullName: 'Yurii Stupnytskyi',
-    linkedIn: 'https://www.linkedin.com'
+    linkedIn:
+      'https://www.linkedin.com/authwall?trk=bf&trkInfo=AQHOHdnXZv7XjQAAAYH8NxAgQgQ9oXLUHcqVUAM7nBEzscBC6wWSgUiUmV7S7SIarlmLTp1NZF7murkAPcU9T4s2dn_4NSGXOEez1pzXWpT589BmRsed-4Lo8XlPN5R1H018ymw=&original_referer=&sessionRedirect=https%3A%2F%2Fwww.linkedin.com%2Fin%2Fyurii-stupnytskyi-980936243%3Flipi%3Durn%253Ali%253Apage%253Ad_flagship3_profile_view_base_contact_details%253BI2GJodxKT0ahJ2IgX%252B00ng%253D%253D'
   },
   {
     photo:
@@ -60,9 +60,8 @@ export const infoAboutDeveloper = [
     linkedIn: 'https://www.linkedin.com'
   },
   {
-    photo:
-      'https://media2.nekropole.info/2013/05/375px-Vasco_da_Gama_-_1838.png',
+    photo: '/photo/Kulesh.jpg',
     fullName: 'Oleksandr Kulesh ',
-    linkedIn: 'https://www.linkedin.com'
+    linkedIn: 'https://www.linkedin.com/in/oleksandr-kulesh-b74640178/'
   }
 ];
