833f8acc5fdfd8825ad0d023f8798f734b9ff5e2
Yurii Stupnytskiy <wedmezin@gmail.com>
2022-07-15 11:11:13 +0300

    add naming

--- frontend/src/Tests/HowToAddLocation.test.tsx
+++ frontend/src/Tests/HowToAddLocation.test.tsx
@@ -3,18 +3,18 @@ import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
 import HowToAddLocation from '../components/HowToAddLocation/HowToAddLocation';
 
-describe('Tests', () => {
-  test('Text', () => {
+describe('How To Add Location Tests', () => {
+  test('Text test', () => {
     render(<HowToAddLocation />);
     const howTo = screen.getByText(/navBar.howToUse/i);
     expect(howTo).toBeInTheDocument();
   });
-  test('Button', () => {
+  test('Button test', () => {
     render(<HowToAddLocation />);
     const btn = screen.getByTestId('button-open');
     expect(btn).toBeInTheDocument();
   });
-  test('Click to check title', () => {
+  test('Click to check title test', () => {
     render(<HowToAddLocation />);
     const btn = screen.getByTestId('button-open');
     expect(screen.queryByTestId('dialog-title')).toBeNull();
