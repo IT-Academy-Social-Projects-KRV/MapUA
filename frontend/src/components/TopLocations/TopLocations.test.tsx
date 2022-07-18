f39a6c37221ca29b460d97af9749eb45b5ced2bc
romawht22 <88610270+romawht22@users.noreply.github.com>
2022-07-18 12:01:06 +0300

    F ix/test warnings (#237)
    * Add basic test structure
    
    * Fix reactDom and 18n warnings
    
    * Fix braces

--- frontend/src/components/TopLocations/TopLocations.test.tsx
+++ frontend/src/components/TopLocations/TopLocations.test.tsx
@@ -6,27 +6,33 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import TopLocations from './TopLocations';
+import i18n from '../../i18n';
 
 describe('TopLocations', () => {
-  const initialState = {
-    topLocations: {
-      loading: false,
-      error: null,
-      success: false,
-      data: []
-    }
-  };
-  const middlewares = [thunk];
-  const mockStore = configureStore(middlewares);
-  it('should render TopLocations', () => {
-    const store = mockStore(initialState);
-    const component = render(
-      <Provider store={store}>
-        <Router>
-          <TopLocations />
-        </Router>
-      </Provider>
-    );
-    expect(component).toMatchSnapshot();
+  beforeEach(() => {
+    i18n.init();
+  });
+  describe('TopLocations', () => {
+    const initialState = {
+      topLocations: {
+        loading: false,
+        error: null,
+        success: false,
+        data: []
+      }
+    };
+    const middlewares = [thunk];
+    const mockStore = configureStore(middlewares);
+    it('should render TopLocations', () => {
+      const store = mockStore(initialState);
+      const component = render(
+        <Provider store={store}>
+          <Router>
+            <TopLocations />
+          </Router>
+        </Provider>
+      );
+      expect(component).toMatchSnapshot();
+    });
   });
 });
